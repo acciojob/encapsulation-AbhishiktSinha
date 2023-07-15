8d56f3fd3ea990bc6fed91a8cc8bc52c788341cc
Abhishikt <abhishiktsinha333@gmail.com>
2023-07-16 01:01:50 +0530

    first commit

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,12 @@
 package com.driver;
 
 public class Main {
+    public static void main(String[] args) {
+        RWOnly ob = new RWOnly();
+//        ob.name = "newName";
+//        java: name has private access in com.driver.RWOnly
+
+        ob.setName("newName");
+    }
   
 }
